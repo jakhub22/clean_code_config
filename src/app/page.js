73ff2accfb10ad07d13d4cd839e96ff4d2dffb97
jakhub22 <jargalan.dev@gmail.com>
2023-10-03 15:08:15 +0800

    added font

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,14 @@ import Hello, { Bye } from '@/utils'
 
 export default function Home() {
     return (
-        <div>
+        <div className='p-5'>
             <Nav />
             <h1>Home !!</h1>
             <div>{Hello()}</div>
             <div className='m-2 flex w-full items-center p-2 py-3'>{Bye()}</div>
+            <div className='font-ttCommons text-xl font-bold'>Сайн байна уу ?</div>
+            <div className='font-zonaPro text-xl font-bold'>Сайн байна уу ?</div>
+            <div className='text-xl font-bold'>Сайн байна уу ?</div>
         </div>
     )
 }
