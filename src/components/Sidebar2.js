f8ce578bdd12daf8616b3247a5c9e2383649f5dc
jakhub22 <jargalan.dev@gmail.com>
2023-10-03 10:42:07 +0800

    test 6

--- src/components/Sidebar2.js
+++ src/components/Sidebar2.js
@@ -1,6 +1,13 @@
-import Hello, { Bye } from '@/utils'
 import React from 'react'
 
+import Hello, { Bye } from '@/utils'
+
 export default function Sidebar() {
-    return <div><h2>Sidebar</h2><div>{Hello()}</div><div>{Bye()}</div></div>
+    return (
+        <div>
+            <h2>Sidebar</h2>
+            <div>{Hello()}</div>
+            <div>{Bye()}</div>
+        </div>
+    )
 }
